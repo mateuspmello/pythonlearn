9a1901535c908082a779c5ac19309c604f40caa8
mateuspmello <mateuspmello@gmail.com>
2020-11-05 23:15:15 -0300

    fim do primeiro arquivo de python

--- primeiro.py
+++ primeiro.py
@@ -1,3 +1,4 @@
+from string import Template 
 
 for num in [12,13,14]:
     if num % 3 == 0:
@@ -5,7 +6,7 @@ for num in [12,13,14]:
     else:
         print('o numero não é', num)
 
-temp = int(input('Entre com a temperatura'))
+temp = 20
 
 if temp < 0:
     print('Congelando')
@@ -20,4 +21,18 @@ for n in range(2,100,2):
     print(n)
 else:
     print('nao')
-    
+
+musicos = [('axel','cantor','feliz'),
+('kurt','cantor','triste')]
+
+#msg = '{1} é {2} e está {3}'
+
+#for nome, profissao, estado in musicos:
+#    print(msg.format(nome, profissao, estado))
+
+st = Template('$aviso aconteceu em $quando')
+
+s = st.substitute({'aviso':'Falta de Eletrecidade',
+'quando':'03 de abril de 2022'})
+
+print(s)
