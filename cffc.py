4cb288c9686ecc2bfd3381606cb4d36c84f2e4d2
mateuspmello <mateuspmello@gmail.com>
2020-11-18 22:43:00 -0300

    modificações

--- cffc.py
+++ cffc.py
@@ -8,12 +8,35 @@ def fc(tempf):
 
 def primo(lista):
     for l in lista:
-        count = 2
+        count = 1
+        primo = 1
         while l > count:
-            if l % count == 1:
-                print('Número ',l,' é primo')
+
+            if count == 1 or count == l:
+                count = count + 1
+                continue
+            else:
+                if l % count == 0:
+                    count = l
+                    count = count + 1
+                    primo = 0
+                    continue 
+            
             count = count + 1
 
-print(cf(float(input('Insira temperatura em celsius:'))))
-print(fc(float(input('Insira temperatura em celsius:'))))
-primo(range(100))
+        if primo == 1:
+            print(l,'é primo')
+            
+def unir_lista(l):
+    lista1 = ()
+    for l1 in l:
+        lista1 += l1
+    return lista1
+
+#print(cf(float(input('Insira temperatura em celsius:'))))
+#print(fc(float(input('Insira temperatura em celsius:'))))
+primo(range(20))
+li = [(1,23),
+    (1,2,3,4),
+    (45,6,7,8,98,6,1)]
+print(unir_lista(li))
---
+++ file.py
@@ -0,0 +1,35 @@
+import sys
+import csv
+
+temp = open('temp.txt','w')
+
+for i in range(100):
+    temp.write('%3d\n' % i)
+
+temp.close()
+
+temp = open('temp.txt')
+
+for x in temp:
+    sys.stdout.write(x)
+
+temp.close()
+
+# print (open('temp.txt').readlines())
+
+dt = (
+    ('temperatura',15,'C','10:40','2020-01-01'),
+    ('temperatura',15,'C','10:40','2020-01-01')
+)
+
+cso = open('cs.csv','w')
+fl = csv.writer(cso)
+fl.writerows(dt)
+cso.close()
+
+
+with open('cs.csv','r') as arquivo_csv:
+    leitor = csv.reader(arquivo_csv, delimiter=',')
+    for col in leitor:
+        sys.stdout.write('%s\n' % str(col))
+
